4a2f38b51d5e1772212ded0ec9125363e1f704dd
jachicao <jachicao@uc.cl>
2017-03-14 21:16:28 -0300

    only subtitle for 250 chars

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   validates :title, presence: true
+  validates :subtitle, presence: true, length: { maximum: 250 }
   validates :body, presence: true
   has_many :comments, dependent: :destroy
 end
